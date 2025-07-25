008eeeae660e20a670b5e0fb977a7d80e9f3a25f
DEXTERLEEer <dexterfiysa@gmail.com>
2025-07-25 03:34:20 -0700

    Delete dexter_cleaner_app directory

--- dexter_cleaner_app/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Starting DexterOptiClean..."
-
-# Always show the error message
-echo "Error: An error occurred while requesting admin privileges."
-
-# Start the application
-python app.py "$@"
