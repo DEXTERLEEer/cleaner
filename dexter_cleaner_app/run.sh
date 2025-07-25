1bc27aaa5af0a60846efa4e90ea005396dc5aafd
DEXTERLEEer <dexterfiysa@gmail.com>
2025-07-25 03:19:45 -0700

    Add files via upload

---
+++ dexter_cleaner_app/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Starting DexterOptiClean..."
+
+# Always show the error message
+echo "Error: An error occurred while requesting admin privileges."
+
+# Start the application
+python app.py "$@"
